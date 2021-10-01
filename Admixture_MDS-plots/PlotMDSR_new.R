c0166acebc85f73753bf36862cfd1d9110dd5369
Shyam G <shyam.g@gmail.com>
2021-10-01 11:00:49 +0200

    New MDS plot and code with country codes

--- Admixture_MDS-plots/PlotMDSR_new.R
+++ Admixture_MDS-plots/PlotMDSR_new.R
@@ -1,28 +1,31 @@
 # plot mds 
 
-setwd("/Users/cvb871/Dropbox/Plantago/GBS/MDS/")
+setwd("~/Work/plantago/Admixture_MDS-plots/")
 distMatrix = read.table("PmajorNoBlanks_6Discarded.dist", skip = 2, nrows = 385, as.is=T)
 sampNames=distMatrix[,1]
 distMatrix = distMatrix[,-1]
 
 mdsFit = cmdscale(d=distMatrix, k=12, eig=TRUE)
 mds = mdsFit$points
-colorcode = read.table("PmajorNoBlanks_6Discarded_mismatches5_coloursk6.list", as.is=T)
+colorcode = read.table("PmajorNoBlanks_6Discarded_mismatches5_coloursK6.list", as.is=T)
 colors = colorcode$V2
 popn = colorcode$V1
+countrycode = read.table("CountryCodes_MDS_plots.csv", header=T, sep=";", as.is=T)
+colorcode$V3 = sapply(colorcode$V1, function(x) countrycode[which(countrycode$Site == x), 5] )
 makeTransparent<-function(someColor, alpha=100)
 {
   newColor<-col2rgb(someColor)
   apply(newColor, 2, function(curcoldata){rgb(red=curcoldata[1], green=curcoldata[2],
                                               blue=curcoldata[3],alpha=alpha, maxColorValue=255)})
 }
-colorback = makeTransparent(colors, 120)
-colorpop = tapply(colors, popn, function(x) {unique(x)})
-pdf(file="PmajorNoBlanks_20REF_6Discarded.MDS.6coordsK6.pdf")
+colorback = makeTransparent(colors, 50)
+colorpop = tapply(colors, popn, function(x) { unique(x) } )
+names(colorpop) = sapply(names(colorpop), function(x) countrycode[which(countrycode$Site == x), 5] )
+#pdf(file="PmajorNoBlanks_20REF_6Discarded.MDS.6coordsK6.pdf")
 par(mar=c(5,4,4,2)+0.1, oma=c(0,0,0,0))
 layout(1)
 plot(mds[,1], mds[,2], type="n", xlab="Coordinate 1", ylab="Coordinate 2")
-text(mds[,1], mds[,2], label=colorcode$V1, cex=0.8, col=colorback, font=1)
+text(mds[,1], mds[,2], label=colorcode$V3, cex=0.8, col=colorback, font=1)
 text(tapply(mds[,1], popn, mean), tapply(mds[,2], popn, mean), label=names(colorpop), 
      cex=1.1, col=colorpop, font=2)
 #points(tapply(mds[,1], popn, mean), tapply(mds[,2], popn, mean), pch=21, cex=1.5, bg=colorpop, col="black")
